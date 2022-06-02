233271f67c05dff2b39847f2d29fc154b1645b25
Hernan <71404736+hlopezbl@users.noreply.github.com>
2022-06-01 22:38:19 -0400

    Fix

--- BL_Cybersec.sh
+++ BL_Cybersec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm BL_Cybersec_install.sh
-wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/main/BL_Cybersec_install.sh
+sudo rm BL_Cybersec_install.sh
+sudo wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/main/BL_Cybersec_install.sh
 sudo chmod +x BL_Cybersec_install.sh
 sh BL_Cybersec_install.sh
--- BL_Cybersec_install.sh
+++ BL_Cybersec_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm BL_Cybersec_update.sh
-wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/main/BL_Cybersec_update.sh
+sudo rm BL_Cybersec_update.sh
+sudo wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/main/BL_Cybersec_update.sh
 sudo chmod +x BL_Cybersec_update.sh
 sh BL_Cybersec_update.sh
