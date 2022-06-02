78ef8a3480de1bb83465046bcd57932b8283a70d
Hernan <71404736+hlopezbl@users.noreply.github.com>
2022-06-01 21:26:49 -0400

    Installer

---
+++ BL_Cybersec_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm BL_Cybersec_install.sh
+wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/89ccaf3e95472c2a816236339f094b27d01111ea/BL_Cybersec_install.sh
+sudo chmod +x BL_Cybersec_install.sh
+sh BL_Cybersec_install.sh
