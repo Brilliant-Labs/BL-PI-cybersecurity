a5398528d0d4efea4e09ddcbcb65df6003cd0bf4
Hernan <71404736+hlopezbl@users.noreply.github.com>
2022-06-02 15:33:58 -0400

    Fix

--- BL_Cybersec_install.sh
+++ BL_Cybersec_install.sh
@@ -9,17 +9,32 @@ sudo rm BL_Cybersec_update_1.sh
 sudo rm BL_Cybersec_update_2.sh
 sudo rm BL_Cybersec_update_3.sh
 sudo rm BL_Cybersec_update_4.sh
-sudo rm BL_Cybersec_db.sh
+sudo rm BL_Cybersec_update_5.sh
+sudo rm BL_Cybersec_update_6.sh
+sudo rm BL_Cybersec_update_7.sh
+sudo rm BL_Cybersec_update_8.sh
+sudo rm BL_Cybersec_update_9.sh
+sudo rm BL_Cybersec_update_10.sh
 sudo wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/main/BL_Cybersec_update_1.sh
 sudo wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/main/BL_Cybersec_update_2.sh
 sudo wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/main/BL_Cybersec_update_3.sh
 sudo wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/main/BL_Cybersec_update_4.sh
-sudo wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/main/BL_Cybersec_db.sh
+sudo wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/main/BL_Cybersec_update_5.sh
+sudo wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/main/BL_Cybersec_update_6.sh
+sudo wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/main/BL_Cybersec_update_7.sh
+sudo wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/main/BL_Cybersec_update_8.sh
+sudo wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/main/BL_Cybersec_update_9.sh
+sudo wget https://raw.githubusercontent.com/Brilliant-Labs/BL-PI-cybersecurity/main/BL_Cybersec_update_10.sh
 sudo chmod +x BL_Cybersec_update_1.sh
 sudo chmod +x BL_Cybersec_update_2.sh
 sudo chmod +x BL_Cybersec_update_3.sh
 sudo chmod +x BL_Cybersec_update_4.sh
-sudo chmod +x BL_Cybersec_db.sh
+sudo chmod +x BL_Cybersec_update_5.sh
+sudo chmod +x BL_Cybersec_update_6.sh
+sudo chmod +x BL_Cybersec_update_7.sh
+sudo chmod +x BL_Cybersec_update_8.sh
+sudo chmod +x BL_Cybersec_update_9.sh
+sudo chmod +x BL_Cybersec_update_10.sh
 
 
 # sh BL_Cybersec_update_1.sh
---
+++ BL_Cybersec_update_5.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt install -y mariadb-server-10.0
+# echo "GRANT ALL PRIVILEGES ON *.* TO bl@localhost IDENTIFIED BY 'BLcybersec';" | mysql -u root -p -h db_name
