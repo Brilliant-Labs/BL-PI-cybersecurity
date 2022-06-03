306ac901e0451f0af35cb500590a9ca53bcf1765
Hernan <71404736+hlopezbl@users.noreply.github.com>
2022-06-02 21:15:59 -0400

    Fix

--- BL_Cybersec_update_3.sh
+++ BL_Cybersec_update_3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+#run this line in console before run this script
+#bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 sudo systemctl enable nodered.service
 node-red-start
 node-red-restart
