2d583a510b2e6d53e966271aff7e8676df96fa0c
Hernan <71404736+hlopezbl@users.noreply.github.com>
2022-06-02 23:27:18 -0400

    Fix

--- BL_Cybersec_update_6.sh
+++ BL_Cybersec_update_6.sh
@@ -2,10 +2,9 @@
 
 sudo apt install -y mariadb-server-10.0
 sudo apt install -y python3-mysqldb 
-sudo mysql_secure_installation
-
 
 #*******  execute manually
+#sudo mysql_secure_installation
 #sudo mysql -u root -p
 #GRANT ALL PRIVILEGES ON *.* TO bl@localhost IDENTIFIED BY 'BLcybersec';
 #GRANT ALL PRIVILEGES ON *.* TO phpmyadmin@localhost IDENTIFIED BY 'BLcybersec'; 
